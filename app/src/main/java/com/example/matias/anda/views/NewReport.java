3c1c1a4e4c214424ce966fd5a41a13a59ea27441
Sebastián Meneses <sebastian.meneses.n@usach.cl>
2015-12-29 16:22:56 -0300

    Se agrega un mapa en el layout de reporte, y el codigo necesario en NewReport.java. Aun no funciona, pero no afecta el funcionamiento de la aplicación.

--- app/src/main/java/com/example/matias/anda/views/NewReport.java
+++ app/src/main/java/com/example/matias/anda/views/NewReport.java
@@ -26,6 +26,15 @@ import com.example.matias.anda.controllers.UploadCouldinary;
 import com.example.matias.anda.utilities.GPSTracker;
 import com.example.matias.anda.utilities.JsonHandler;
 import com.example.matias.anda.utilities.SystemUtilities;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.concurrent.ExecutionException;
@@ -62,13 +71,49 @@ public class NewReport extends Fragment implements View.OnClickListener {
         this.context = context;
 
     }
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     /** Método que crea la vista del fragmento */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // inflat and return the layout
+        View v = inflater.inflate(R.layout.fragment_new_report, container, false);
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
 
-        return inflater.inflate(R.layout.fragment_new_report, container, false);
+        mMapView.onResume();// needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mMapView.getMap();
+        // latitude and longitude
+        double latitude = 17.385044;
+        double longitude = 78.486671;
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Hello Maps");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(17.385044, 78.486671)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        // Perform any camera updates here
+
+        return v;
     }// End onCreateView
 
     /** Método que se ejecuta luego de que el fragmento es creado */
@@ -76,7 +121,9 @@ public class NewReport extends Fragment implements View.OnClickListener {
     public void onResume() {
         super.onResume();
 
+
         if (flag == 1) {
+            mMapView.onResume();
             // Obtener el key enviado desde la clase Reports
             String key = getArguments().getString("key");
             JsonHandler jh = new JsonHandler();
