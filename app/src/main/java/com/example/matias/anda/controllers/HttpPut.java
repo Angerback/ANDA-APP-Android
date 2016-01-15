d09211efaec2d349e7e8548613d0dffd804a02c2
chocolito <matias.lazos@gmail.com>
2016-01-15 13:36:27 -0300

    consumo put, incompleto.

---
+++ app/src/main/java/com/example/matias/anda/controllers/HttpPut.java
@@ -0,0 +1,82 @@
+package com.example.matias.anda.controllers;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Created by Matias on 15-01-2016.
+ */
+public class HttpPut extends AsyncTask<String, Void, String>{
+
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        try {
+            URL url = new URL(params[0]);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setReadTimeout(100000);
+            connection.setConnectTimeout(100000);
+            connection.setRequestProperty("auth_token", params[2]);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestMethod("PUT");
+            connection.setDoInput(true);
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+
+            // Send request
+            DataOutputStream os = new DataOutputStream(
+                    connection.getOutputStream());
+            os.writeBytes(params[1]);
+            os.flush();
+            os.close();
+
+            int status = connection.getResponseCode();
+            if(status >= 400)
+                return "error_responsive";
+
+            // Get resposive
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while((inputLine = in.readLine()) != null)
+                response.append(inputLine);
+            in.close();
+            System.out.println("Responsive :" +response.toString() +"\n");
+
+            return response.toString();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return  null;
+
+    }
+}
+
+
+
+//ReportsDetail
+
+// Obtener el key enviado desde la clase Reports
+/*
+String key = getArguments().getString("key");
+JsonHandler jh = new JsonHandler();
+id = jh.getValor(key, "idUsuario");
+auth_token = jh.getValor(key, "auth_token");*/
+
+
